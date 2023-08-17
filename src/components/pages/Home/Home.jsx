368558ba551e527b20abc52d72a677b8f3a71652
DanielKirchenbauer <kirchenbauer.daniel@gmail.com>
2023-08-17 11:34:02 +0200

    Add Drink startet

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import IngredientItem from "../../shared/IngredientItem/IngredientItem";
+import AddYourDrink from "../AddYourDrink/AddYourDrink";
 
 const Home = () => {
 
   const ingredients = ["Gin","Vodka","Rum","Scotch","Alkoholfrei","Zufall"]
 
   return ( 
+    <>
+    <AddYourDrink/>
     <section>
       {ingredients.map(ingredient => <IngredientItem key={ingredient} ingredient={ingredient}/>)}
     </section>
+    </>
   );
 }
 
